67c337de14a14bae31939d9ab12df1b3da7b8cd4
Dusan Milosavljevic <dusan.milosavljevic.1624@gmail.com>
2016-06-25 02:08:38 +0200

    Enabling unit testing

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,42 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint');
-
+    jshint = require('gulp-jshint'),
+    testem = require('gulp-testem'),
+    http = require('http');
 gulp.task('lint', function () {
-  return gulp.src('./src/**/*.js')
+  return gulp.src(['./src/**/*.js', './test/**/*.js'])
         .pipe(jshint({
           globals: {
             _: false,
-            $: false
-          },
-          browser: true,
-          devel: true
+            $: false,
+            jasmine: false,
+            browser: true,
+            devel: true,
+            describe: false,
+            it: false,
+            expect: false,
+            beforeEach: false,
+            afterEach: false,
+            sinon: false
+          }
         }))
-        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('default'));
+        console.log('linter was run');
+});
+
+gulp.task('coverage', function () {
+  var coverageServer = http.createServer(function (req, resp) {
+        req.pipe(fs.createWriteStream('coverage.json'))
+        resp.end()
+    });
+
+    var port = 7358;
+    coverageServer.listen(port);
+    console.log("Coverage Server Started on port", port);
+});
+
+gulp.task('testem', ['coverage', 'lint'], function () {
+  gulp.src([''])
+      .pipe(testem({
+        configFile: 'testem.json'
+      }));
 });
---
+++ test/sayHello_spec.js
@@ -0,0 +1,5 @@
+describe('hello', function () {
+  it('should say hello', function () {
+      expect(sayHello()).toBe('Hello!');
+  });
+});
