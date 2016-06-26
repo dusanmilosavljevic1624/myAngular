d0198b6db2cc589754aab52d7c50f990e9462e5d
Dusan Milosavljevic <dusan.milosavljevic.1624@gmail.com>
2016-06-26 13:16:42 +0200

    Implementing scope phases which reflect the current phase of the cycle

--- src/scope.js
+++ src/scope.js
@@ -4,6 +4,7 @@ function Scope() {
   this.$$watchers = [];
   this.$$lastDirtyWatch = null;
   this.$$asyncQueue = [];
+  this.$$phase = null;
 }
 
 Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
@@ -40,6 +41,7 @@ Scope.prototype.$digest = function () {
   var ttl = 10;
   var dirty;
   this.$$lastDirtyWatch = null;
+  this.$beginPhase('$digest');
   do {
     while(this.$$asyncQueue.length) {
       var asyncTask = this.$$asyncQueue.shift();
@@ -47,9 +49,11 @@ Scope.prototype.$digest = function () {
     }
     dirty = this.$$digestOnce();
     if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
+      this.$clearPhase();
       throw "10 digest iterations reached";
     }
   } while (dirty || this.$$asyncQueue.length);
+  this.$clearPhase();
 };
 
 Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
@@ -68,12 +72,25 @@ Scope.prototype.$eval = function (expr, locals) {
 
 Scope.prototype.$apply = function (expr) {
   try {
+    this.$beginPhase('$apply');
     this.$eval(expr);
   } finally {
+    this.$clearPhase();
     this.$digest();
   }
 };
 
 Scope.prototype.$evalAsync = function (expr) {
   this.$$asyncQueue.push({scope: this, expression: expr});
+};
+
+Scope.prototype.$beginPhase = function (phase) {
+  if(this.$$phase){
+    throw this.$$phase + 'already in progress.';
+  }
+  this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function () {
+  this.$$phase = null;
 };
