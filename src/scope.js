ef4faa4b3ec28bfef91915ddd67ac8f00d90f492
Dusan Milosavljevic <dusan.milosavljevic.1624@gmail.com>
2016-06-28 11:19:50 +0200

    Enable creation of child scopes

--- src/scope.js
+++ src/scope.js
@@ -210,3 +210,10 @@ Scope.prototype.$watchGroup = function (watchFns, listenerFn) {
     });
   }
 };
+
+Scope.prototype.$new = function () {
+  var ChildScope = function () {};
+  ChildScope.prototype = this;
+  var child = new ChildScope();
+  return child;
+};
